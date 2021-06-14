71b4d3271512364b4d2df8f79e04c99c3ddd8dbc
jaime-medhome <jaime.sanchez@med-home.es>
2021-06-14 15:43:07 +0200

    initial firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:carrot_app/pages/root_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +10,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Carrot Locations',
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-      ),
-      home: MyHomePage(title: 'Carrot locations'),
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            return MaterialApp(home: Center(child: Text('Ha ocurrido un problema al iniciar la aplicación')));
+          }
+          if (snapshot.connectionState == ConnectionState.done){
+            return MaterialApp(
+              title: 'Carrot Locations',
+              theme: ThemeData(
+                primarySwatch: Colors.deepOrange,
+              ),
+              home: MyHomePage(title: 'Carrot locations'),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
     );
   }
 }
--- lib/pages/places_page.dart
+++ lib/pages/places_page.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PlacesPage extends StatelessWidget {
   const PlacesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('__PLACES'),
+    return Scaffold(
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('/markers').orderBy('name').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            try {
+              if (!snapshot.hasData) return const Center(child: CircularProgressIndicator());
+              return ListView.builder(
+                itemCount: snapshot.data?.docs.length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(snapshot.data?.docs[index]['name']),
+                  subtitle: Text(
+                    'Latitude: ${snapshot.data?.docs[index]['lat']} - Longitude: ${snapshot.data?.docs[index]['lng']}',
+                  ),
+                ),
+              );
+            } catch (e) {
+              return Center(
+                child: ListTile(
+                  title: Text(
+                    'Se ha producido un error. Comprueba tu conexión y vuelve a intentarlo',
+                    style: TextStyle(color: Colors.grey.shade700),
+                  ),
+                ),
+              );
+            }
+          }),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
