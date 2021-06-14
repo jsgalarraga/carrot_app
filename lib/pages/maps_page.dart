c69a5fada5ff3f3a87880c6b656b3156dea50873
jaime-medhome <jaime.sanchez@med-home.es>
2021-06-14 11:59:13 +0200

    added map

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapsPage extends StatelessWidget {
   const MapsPage({Key? key}) : super(key: key);
@@ -6,7 +9,34 @@ class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('__MAPS'),
+      child: MapWidget(),
+    );
+  }
+}
+
+class MapWidget extends StatefulWidget {
+  @override
+  State<MapWidget> createState() => MapWidgetState();
+}
+
+class MapWidgetState extends State<MapWidget> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kCarrotsLab = CameraPosition(
+    target: LatLng(41.375969168654926, 2.186781542297567),
+    zoom: 14,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kCarrotsLab,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
