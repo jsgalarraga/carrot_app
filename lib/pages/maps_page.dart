a0c152c365f0d51832d27101fe62fd0cea724da6
jaime-medhome <jaime.sanchez@med-home.es>
2021-06-14 13:15:04 +0200

    added location

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapsPage extends StatelessWidget {
   const MapsPage({Key? key}) : super(key: key);
@@ -21,22 +22,75 @@ class MapWidget extends StatefulWidget {
 
 class MapWidgetState extends State<MapWidget> {
   Completer<GoogleMapController> _controller = Completer();
+  Location location = Location();
+  LocationData? _currentPosition;
+  LatLng _initialLatLng = LatLng(41.375969168654926, 2.186781542297567);
+  bool _firstLocation = true;
 
-  static final CameraPosition _kCarrotsLab = CameraPosition(
-    target: LatLng(41.375969168654926, 2.186781542297567),
-    zoom: 14,
-  );
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _kCarrotsLab,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+        initialCameraPosition: CameraPosition(
+          target: _initialLatLng,
+          zoom: 14,
+        ),
+        onMapCreated: _onMapCreated,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
       ),
     );
   }
+
+  getLocation() async{
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _currentPosition = await location.getLocation();
+    _initialLatLng = LatLng(_currentPosition!.latitude!,_currentPosition!.longitude!);
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      setState(() {
+        _currentPosition = currentLocation;
+        _initialLatLng = LatLng(_currentPosition!.latitude!,_currentPosition!.longitude!);
+      });
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    location.onLocationChanged.listen((l) {
+      if (_firstLocation) {
+        _firstLocation = false;
+        controller.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(target: LatLng(l.latitude!, l.longitude!), zoom: 14),
+          ),
+        );
+      }
+    });
+    _controller.complete(controller);
+  }
+
 }
