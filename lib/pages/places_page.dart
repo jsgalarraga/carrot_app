5c223c8b6402375a273048c54ea7816a03e1cc5c
jaime-medhome <jaime.sanchez@med-home.es>
2021-06-14 17:04:41 +0200

    add marker

--- lib/pages/places_page.dart
+++ lib/pages/places_page.dart
@@ -34,8 +34,96 @@ class PlacesPage extends StatelessWidget {
           }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          showDialog(context: context, builder: (BuildContext context) => AddPlaceDialog());
+        },
       ),
     );
+  }
+}
+
+class AddPlaceDialog extends StatefulWidget {
+  const AddPlaceDialog({Key? key}) : super(key: key);
+
+  @override
+  _AddPlaceDialogState createState() => _AddPlaceDialogState();
+}
+
+class _AddPlaceDialogState extends State<AddPlaceDialog> {
+  final _nameController = TextEditingController();
+  final _latController = TextEditingController();
+  final _lngController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Add new marker"),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: _nameController,
+              keyboardType: TextInputType.text,
+              textCapitalization: TextCapitalization.sentences,
+              decoration: InputDecoration(labelText: 'Marker name'),
+              validator: (value){
+                if (value!.isEmpty) return 'Please, enter a marker name';
+              },
+            ),
+            TextFormField(
+              controller: _latController,
+              decoration: InputDecoration(labelText: 'Latitude', hintText: '12.345'),
+              keyboardType: TextInputType.number,
+              validator: (value){
+                if (value!.isEmpty) return 'Please, enter a latitude';
+                if (double.tryParse(value) == null)
+                  return 'Please, enter a valid latitude';
+              },
+            ),
+            TextFormField(
+              controller: _lngController,
+              decoration: InputDecoration(labelText: 'Longitude', hintText: '12.345'),
+              keyboardType: TextInputType.number,
+              validator: (value){
+                if (value!.isEmpty) return 'Please, enter a longitude';
+                if (double.tryParse(value) == null)
+                  return 'Please, enter a valid longitude';
+              },
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          child: Text("Cancel"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          child: Text("Save"),
+          onPressed: () {
+            if (_formKey.currentState!.validate()){
+              _addMarker();
+              Navigator.of(context).pop();
+            }
+          },
+        ),
+      ],
+    );
+  }
+
+  void _addMarker(){
+    CollectionReference markers = FirebaseFirestore.instance.collection('markers');
+    markers.add({
+      'name': _nameController.text,
+      'lat': double.parse(_latController.text),
+      'lng': double.parse(_lngController.text),
+    }).catchError((error) {
+      print("Failed to add marker: $error");
+    });
   }
 }
